334f672261ce42c4003196efdd74b1e76c920dcd
Nguyễn Phan Nam <73817858+nguyenphannam2223@users.noreply.github.com>
2024-04-07 21:20:06 +0700

    Create CardManager.java

---
+++ src/CardManager.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class CardManager {
+    private List<InsuranceCard> cards;
+
+    public CardManager() {
+        this.cards = new ArrayList<>();
+    }
+
+    public void addCard(InsuranceCard card) {
+        cards.add(card);
+    }
+
+    public void removeCard(String cardNumber) {
+        cards.removeIf(card -> card.getCardNumber().equals(cardNumber));
+    }
+
+    public InsuranceCard getCardByNumber(String cardNumber) {
+        for (InsuranceCard card : cards) {
+            if (card.getCardNumber().equals(cardNumber)) {
+                return card;
+            }
+        }
+        return null;
+    }
+}
